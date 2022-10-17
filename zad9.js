8f2a7276cd525f89d29276ee7ae411addc4cbe05
Konrad Trzeciak <trzeciak.konrad96@gmail.com>
2022-10-17 19:53:28 +0200

    first commit

---
+++ zad9.js
@@ -0,0 +1,19 @@
+//
+const input = process.argv[2];
+if (input == undefined)
+{
+    console.log("Hello World");
+}
+else
+{
+    console.log("Hello " + input);
+}
+
+
+
+// odwrotna kolejnosc Ala ma kota i psa
+// console.log(process.argv[6]);
+// console.log(process.argv[5]);
+// console.log(process.argv[4]);
+// console.log(process.argv[3]);
+// console.log(process.argv[2]);
