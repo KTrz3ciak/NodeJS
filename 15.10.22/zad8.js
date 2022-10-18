dc8647ec7b7035718f489c53a690016a52a26064
Konrad Trzeciak <trzeciak.konrad96@gmail.com>
2022-10-18 18:31:50 +0200

    new files

---
+++ 15.10.22/zad8.js
@@ -0,0 +1,18 @@
+// Rozszerzenie zadania 4. Aplikacja powinna wczytać jedną liczbę z pliku a.txt, drugą liczbę z pliku b.txt. Na tych liczbach należy wykonać operacja dodawania, odejmowania, mnożenia i dzielenia a wyniki wszystkich działań zapisać w pliku wynik.txt, każdy wynik w osobnej linii. Należy wykorzystać moduł Core'owy File system (https://nodejs.org/dist/latest-v16.x/docs/api/fs.html , szukana funkcja ma przyrostek Sync)
+const fs = require('fs');
+// const numberA = require('a.txt');
+// const numberB = require('b.txt');
+fs.readFileSync('a.txt', (err, data) => {
+    if (err) throw err;
+    console.log(data);
+});
+console.log('Obecna wartość: ' + numberA);
+
+let wynik = "";
+    wynik += "dodawanie 1 + 2 = " + mat.add(numberA,numberB);
+    wynik += "\nodejmowanie 2 - 1 = " + mat.sub(numberA,numberB);
+    wynik += "\nmnożenie 2 * 2 = " + mat.mul(numberA,numberB);
+    wynik += "\ndzielenie 4 / 2 = " + mat.div(numberA,numberB);
+    
+
+fs.writeFileSync("wynik.txt", wynik);
