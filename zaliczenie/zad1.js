c270dd341d210cdba9df01ff382ce7467f2018ec
Konrad Trzeciak <trzeciak.konrad96@gmail.com>
2023-01-26 21:04:04 +0100

    adding a comment at the beginning

--- zaliczenie/zad1.js
+++ zaliczenie/zad1.js
@@ -1,3 +1,6 @@
+// 1. [2 punkty] Napisz jak najprostszy kod który spowoduje błąd stack overflow,
+// czyli zwróci komunikat błędu: Uncaught RangeError: Maximum call stack size exceeded
+
 function firstItem() {
     secondItem();
 }
