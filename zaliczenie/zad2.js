baf0ed4772b4bcc0f3e3d9c01a6573f5afe19afe
Konrad Trzeciak <trzeciak.konrad96@gmail.com>
2023-01-28 19:44:10 +0100

    Task 2 preparation

---
+++ zaliczenie/zad2.js
@@ -0,0 +1,5 @@
+// 2. [2 punkty] Napisz aplikację która przyjmuje w parametrze uruchamiania ciąg znaków a następnie wyświetli go w kolorach tęczy. Wykorzystaj moduł colors (https://www.npmjs.com/package/colors) w wersji 1.3.2!. Pamiętaj o obsłudze błędów.
+
+var colors = require('colors');
+let napis = "BestNode";
+console.log(napis.rainbow);
