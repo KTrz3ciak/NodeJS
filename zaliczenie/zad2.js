cd172322bdf799507fa6012fe514e3e4a1b42fe8
Konrad Trzeciak <trzeciak.konrad96@gmail.com>
2023-01-28 20:45:46 +0100

    Variable changed

--- zaliczenie/zad2.js
+++ zaliczenie/zad2.js
@@ -1,8 +1,8 @@
 // 2. [2 punkty] Napisz aplikację która przyjmuje w parametrze uruchamiania ciąg znaków a następnie wyświetli go w kolorach tęczy. Wykorzystaj moduł colors (https://www.npmjs.com/package/colors) w wersji 1.3.2!. Pamiętaj o obsłudze błędów.
 
 var colors = require('colors');
-let anyString = "Node is the best";
-console.log(napis.rainbow);
+const input = process.argv[2];
+console.log(input.rainbow);
 
 if (colors) {
     colors.enable();
